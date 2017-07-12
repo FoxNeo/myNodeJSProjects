5e8c85881d459310103615b5305227a9817f060d
Caceres Miguel <miguelcaceres94@hotmail.com>
2017-07-12 14:22:36 +0200

    Add Layout for the Template Engine Pug and CSS

---
+++ layout/app.js
@@ -0,0 +1,15 @@
+// Sample App server with the framework express
+var express = require("express");
+
+var app = express();
+
+app.set("view engine", "jade");
+
+app.get("/", function(req, res){
+	res.render("index");
+});
+
+app.get("/login", function(req, res){
+	res.render("login");
+});
+app.listen(8080);
